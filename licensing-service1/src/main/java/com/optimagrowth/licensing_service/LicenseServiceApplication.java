568a4e82717cf5d2ec141011b0016a785a3da905
maveltatkeu <maveltatkeu@gmail.com>
2024-05-30 14:06:40 +0100

    new repo

---
+++ licensing-service1/src/main/java/com/optimagrowth/licensing_service/LicenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+
+}
